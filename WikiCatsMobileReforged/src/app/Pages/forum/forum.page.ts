c3d46a031a289e9718c6ba6777d7df8ac880f728
Michael-Encina-Guzman <mi.encina@duocuc.cl>
2024-10-29 07:29:02 -0300

    Navegación y implementación de cámara

--- WikiCatsMobileReforged/src/app/Pages/forum/forum.page.ts
+++ WikiCatsMobileReforged/src/app/Pages/forum/forum.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CatService } from 'src/app/Services/cat.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Router } from '@angular/router';
+import { UserInterface } from 'src/app/Interfaces/user.interface';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-forum',
@@ -8,25 +12,65 @@ import { CatService } from 'src/app/Services/cat.service';
 })
 export class ForumPage implements OnInit {
   catImages: string[] = [];
-  fetchedImages: Set<string> = new Set();
+  fetchedImages: Set<string> = new Set(); 
+  maxImages: number = 4; 
+  capturedImage: string | undefined;
 
-  constructor(private catService: CatService) { }
+  constructor(private catService: CatService, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
     this.fetchCatImages();
   }
 
   fetchCatImages() {
-    this.catService.getCatImages().subscribe((data) => {
-      data.forEach(cat => {
-        if (!this.fetchedImages.has(cat.url)) {
-          this.fetchedImages.add(cat.url);
-          this.catImages.push(cat.url);
+    this.catService.getCatImages().subscribe(
+      (data) => {
+        data.forEach(cat => {
+          if (!this.fetchedImages.has(cat.url)) {
+            this.fetchedImages.add(cat.url);
+            this.catImages.push(cat.url);
+          }
+        });
+
+        if (this.catImages.length < 4 && this.catImages.length < this.maxImages) {
+          this.fetchCatImages();
         }
-      });
-      if (this.catImages.length < 3) {
-        this.fetchCatImages();
+      },
+      (error) => {
+        console.error('Error fetching cat images:', error);
       }
+    );
+  }
+
+  async openCamera() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera 
+    });
+
+    this.capturedImage = image.dataUrl;
+  }
+
+  // Navegación
+
+  Home(){
+    this.router.navigate(['/home'])
+  }
+
+  Profile(){
+    this.router.navigate(['/profile'])
+  }
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        console.log('Logged out successfully');
+        this.router.navigate(['/login']); 
+      },
+      error: (err) => console.error('Logout error', err),
     });
   }
+
 }
--- WikiCatsMobileReforged/src/app/Pages/profile/profile.page.ts
+++ WikiCatsMobileReforged/src/app/Pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  //Navegación
+
+  Home(){
+    this.router.navigate(['/home'])
+  }
+
+  Forum(){
+    this.router.navigate(['/forum'])
+  }
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        console.log('Logged out successfully');
+        this.router.navigate(['/login']); 
+      },
+      error: (err) => console.error('Logout error', err),
+    });
   }
 
 }
